d70b5ea2e172e73cdac9af395fe47f6d3dc0530b
kartik-commits <kartiklashkare@gmail.com>
2025-11-16 18:09:59 +0530

    redwood: Initialize

---
+++ clone_script-redwood.sh
@@ -0,0 +1,8 @@
+rm -rf device/xiaomi/redwood && git clone https://github.com/kartik-commits/android_device_xiaomi_redwood.git device/xiaomi/redwood
+rm -rf device/xiaomi/sm8350-common && git clone https://github.com/kartik-commits/android_device_xiaomi_sm8350-common.git device/xiaomi/sm8350-common
+rm -rf vendor/xiaomi/redwood && git clone https://github.com/kartik-commits/proprietary_vendor_xiaomi_redwood.git vendor/xiaomi/redwood
+rm -rf vendor/xiaomi/sm8350-common && git clone https://github.com/kartik-commits/proprietary_vendor_xiaomi_sm8350-common.git vendor/xiaomi/sm8350-common
+rm -rf vendor/xiaomi/redwood-firmware && git clone https://gitlab.com/kartik-commits/redwood-firmware.git vendor/xiaomi/redwood-firmware
+rm -rf kernel/xiaomi/sm8350 && git clone https://github.com/kartik-commits/kernel_xiaomi_sm7325.git kernel/xiaomi/sm8350 --depth=1
+rm -rf hardware/xiaomi && git clone https://github.com/PixelOS-AOSP/android_hardware_xiaomi.git hardware/xiaomi
+rm -rf hardware/sony/timekeep && git clone https://github.com/LineageOS/android_hardware_sony_timekeep.git hardware/sony/timekeep
